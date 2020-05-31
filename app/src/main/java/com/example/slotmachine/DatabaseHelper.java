64045f6473709c923d6167bbf212a1616daa3fbf
Ruben Gonzalez <rubengaviles@gmail.com>
2020-05-31 20:24:45 +0200

    Register database added

---
+++ app/src/main/java/com/example/slotmachine/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.slotmachine;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "slotmachine_db";
+    public static final String TABLE_NAME = "users";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "USERNAME";
+    public static final String COL3 = "PASSWORD";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        String createTable = "CREATE TABLE "+ TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "USERNAME TEXT, PASSWORD TEXT)";
+        db.execSQL(createTable);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+    }
+
+    public boolean addData(String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,username);
+        contentValues.put(COL3, password);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result==-1){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/slotmachine/RegisterActivity.java
+++ app/src/main/java/com/example/slotmachine/RegisterActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button goBackBtn;
+    private Button goBackBtn, submitBtn;
+    private EditText username, password, repeatPassword;
+    DatabaseHelper slotmachineDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         goBackBtn = findViewById(R.id.goBackBtn);
+        submitBtn = findViewById(R.id.submitRegistrationBtn);
+        username = findViewById(R.id.registerUsername);
+        password = findViewById(R.id.registerPassword);
+        repeatPassword = findViewById(R.id.registerPasswordRepeat);
+        slotmachineDB = new DatabaseHelper(this);
+
         goBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +36,25 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (password.getText().toString().equals(repeatPassword.getText().toString())&& username.length()!=0){
+                    String newUsername;
+                    addUser(username.getText().toString(), password.getText().toString());
+                } else{
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void addUser(String username, String password){
+        boolean insertData = slotmachineDB.addData(username, password);
+        if(insertData){
+            Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_LONG).show();
+        } else{
+            Toast.makeText(RegisterActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
     }
 }
