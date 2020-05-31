712ee949d61dcd009ceefd56e71da5dfdf1b6738
Kiran <kiran.kumullil@students.fhnw.ch>
2020-05-31 21:04:57 +0200

    coin done

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -29,6 +29,10 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
     private RelativeLayout layout;
     private Button colorButton;
 
+    private EditText input;
+    private Button ok_button;
+
+
     ImageView btn_up, btn_down;
     WheelImageView image, image2,image3;
     TextView txt_score;
@@ -54,6 +58,12 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ok_button = (Button) findViewById(R.id.okButton);
+        txt_score = (TextView) findViewById(R.id.txt_score);
+        input = (EditText) findViewById(R.id.input);
+        //ok_button.setOnClickListener(this);
+
+
         colorButton = findViewById(R.id.colorButton);
         layout = findViewById(R.id.bg_color);
 
@@ -62,32 +72,31 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
         music.setClass(this, MusicService.class);
         startService(music);
 
-        btn_down = (ImageView)findViewById(R.id.btn_down);
-        btn_up = (ImageView)findViewById(R.id.btn_up);
+        btn_down = (ImageView) findViewById(R.id.btn_down);
+        btn_up = (ImageView) findViewById(R.id.btn_up);
 
         image = (WheelImageView) findViewById(R.id.image);
         image2 = (WheelImageView) findViewById(R.id.image2);
         image3 = (WheelImageView) findViewById(R.id.image3);
 
-        txt_score = (TextView ) findViewById(R.id.txt_score);
+        txt_score = (TextView) findViewById(R.id.txt_score);
 
 
         image.setEventEnd(MainActivity.this);
         image2.setEventEnd(MainActivity.this);
         image3.setEventEnd(MainActivity.this);
 
-        colorButton.setOnClickListener(new View.OnClickListener(){
+        colorButton.setOnClickListener(new View.OnClickListener() {
             Drawable backgroud = colorButton.getBackground();
 
             @Override
             public void onClick(View view) {
-                if(colorButton.getText().equals("Dark Mode")){
+                if (colorButton.getText().equals("Dark Mode")) {
                     colorButton.setText("Light Mode");
                     layout.setBackgroundResource(R.color.newColor);
-                }
-                else if (colorButton.getText().equals("Light Mode")) {
+                } else if (colorButton.getText().equals("Light Mode")) {
                     colorButton.setText("Dark Mode");
-                    layout.setBackground(backgroud);
+                    layout.setBackgroundResource(R.color.backgroud);
                 }
             }
         });
@@ -99,9 +108,9 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
                     btn_up.setVisibility(View.GONE);
                     btn_down.setVisibility(View.VISIBLE);
 
-                    image.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
-                    image2.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
-                    image3.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
+                    image.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1) + 5);
+                    image2.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1) + 5);
+                    image3.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1) + 5);
 
                     Common.SCORE -= 50;
                     txt_score.setText(String.valueOf(Common.SCORE));
@@ -110,8 +119,17 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
                 }
             }
         });
-    }
 
+        ok_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  int amount = Integer.parseInt(input.getText().toString());
+                    Common.SCORE += amount;
+                    txt_score.setText(String.valueOf(Common.SCORE));
+
+            }
+        });
+    }
     @Override
     public void eventEnd(int result, int count) {
         if(count_done < 2){
