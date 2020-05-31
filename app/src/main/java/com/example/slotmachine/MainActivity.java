b8a3b68457c943126e2dd271effd23e6c6a66413
Dani <danilsson.fernandes@students.fhnw.ch>
2020-05-31 18:58:33 +0200

    Bug fixed after pull request

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.slotmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.content.Intent;
-
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -38,7 +35,6 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
 
     int count_done = 0;
 
-
     private boolean mIsBound = false;
     private MusicService mServ;
     private ServiceConnection Scon =new ServiceConnection(){
@@ -53,100 +49,91 @@ public class  MainActivity extends AppCompatActivity implements IEventEnd {
         }
     };
 
-
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-   }
+        colorButton = findViewById(R.id.colorButton);
+        layout = findViewById(R.id.bg_color);
 
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
 
+        btn_down = (ImageView)findViewById(R.id.btn_down);
+        btn_up = (ImageView)findViewById(R.id.btn_up);
 
-   /*
-        btLogin= (Button)findViewById(R.id.button1);
-        etLoginName = (EditText)findViewById(R.id.editText1);
-        etPassword = (EditText)findViewById(R.id.editText2);
-       colorButton = findViewById(R.id.colorButton);
-       layout = findViewById(R.id.bg_color);
+        image = (WheelImageView) findViewById(R.id.image);
+        image2 = (WheelImageView) findViewById(R.id.image2);
+        image3 = (WheelImageView) findViewById(R.id.image3);
 
-       doBindService();
-       Intent music = new Intent();
-       music.setClass(this, MusicService.class);
-       startService(music);
-
-       btn_down = (ImageView)findViewById(R.id.btn_down);
-       btn_up = (ImageView)findViewById(R.id.btn_up);
-
-       image = (WheelImageView) findViewById(R.id.image);
-       image2 = (WheelImageView) findViewById(R.id.image2);
-       image3 = (WheelImageView) findViewById(R.id.image3);
-
-       txt_score = (TextView ) findViewById(R.id.txt_score);
+        txt_score = (TextView ) findViewById(R.id.txt_score);
 
 
-       image.setEventEnd(MainActivity.this);
-       image2.setEventEnd(MainActivity.this);
-       image3.setEventEnd(MainActivity.this);
+        image.setEventEnd(MainActivity.this);
+        image2.setEventEnd(MainActivity.this);
+        image3.setEventEnd(MainActivity.this);
 
-       colorButton.setOnClickListener(new View.OnClickListener(){
-       Drawable backgroud = colorButton.getBackground();
+        colorButton.setOnClickListener(new View.OnClickListener(){
+            Drawable backgroud = colorButton.getBackground();
 
-           @Override
-           public void onClick(View view) {
-               if(colorButton.getText().equals("Dark Mode")){
-                   colorButton.setText("Light Mode");
-                   layout.setBackgroundResource(R.color.newColor);
-               }
-               else if (colorButton.getText().equals("Light Mode")) {
-                   colorButton.setText("Dark Mode");
-                   layout.setBackground(backgroud);
-               }
-           }
-       });
+            @Override
+            public void onClick(View view) {
+                if(colorButton.getText().equals("Dark Mode")){
+                    colorButton.setText("Light Mode");
+                    layout.setBackgroundResource(R.color.newColor);
+                }
+                else if (colorButton.getText().equals("Light Mode")) {
+                    colorButton.setText("Dark Mode");
+                    layout.setBackground(backgroud);
+                }
+            }
+        });
 
-       btn_up.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if (Common.SCORE >= 50) {
-                   btn_up.setVisibility(View.GONE);
-                   btn_down.setVisibility(View.VISIBLE);
+        btn_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Common.SCORE >= 50) {
+                    btn_up.setVisibility(View.GONE);
+                    btn_down.setVisibility(View.VISIBLE);
 
-                   image.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
-                   image2.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
-                   image3.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
+                    image.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
+                    image2.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
+                    image3.setValueRandom(new Random().nextInt(6), new Random().nextInt((15 - 5) + 1)+5);
 
-                   Common.SCORE -= 50;
-                   txt_score.setText(String.valueOf(Common.SCORE));
-               } else {
-                   Toast.makeText(MainActivity.this, "Not enough Money", Toast.LENGTH_SHORT).show();
-               }
-           }
-       });
-   }
+                    Common.SCORE -= 50;
+                    txt_score.setText(String.valueOf(Common.SCORE));
+                } else {
+                    Toast.makeText(MainActivity.this, "Not enough Money", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     @Override
     public void eventEnd(int result, int count) {
-       if(count_done < 2){
-           count_done++;
-       }else{
-           btn_down.setVisibility(View.GONE);
-           btn_up.setVisibility(View.VISIBLE);
+        if(count_done < 2){
+            count_done++;
+        }else{
+            btn_down.setVisibility(View.GONE);
+            btn_up.setVisibility(View.VISIBLE);
 
-           count_done= 0; // reset
+            count_done= 0; // reset
 
-           if (image.getValue()== image2.getValue() && image2.getValue() == image3.getValue()){
-               Toast.makeText(this, "You win big prize", Toast.LENGTH_SHORT).show();
-               Common.SCORE += 300;
-               txt_score.setText(String.valueOf(Common.SCORE));
-           }else if (image.getValue() == image2.getValue() || image2.getValue() == image3.getValue() || image.getValue() == image3.getValue()){
-               Toast.makeText(this, "You win small prize", Toast.LENGTH_SHORT).show();
-               Common.SCORE += 300;
-               txt_score.setText(String.valueOf(Common.SCORE));
-           }else{
-               Toast.makeText(this, "You lose", Toast.LENGTH_SHORT).show();
-           }
-       }
+            if (image.getValue()== image2.getValue() && image2.getValue() == image3.getValue()){
+                Toast.makeText(this, "You win big prize", Toast.LENGTH_SHORT).show();
+                Common.SCORE += 300;
+                txt_score.setText(String.valueOf(Common.SCORE));
+            }else if (image.getValue() == image2.getValue() || image2.getValue() == image3.getValue() || image.getValue() == image3.getValue()){
+                Toast.makeText(this, "You win small prize", Toast.LENGTH_SHORT).show();
+                Common.SCORE += 300;
+                txt_score.setText(String.valueOf(Common.SCORE));
+            }else{
+                Toast.makeText(this, "You lose", Toast.LENGTH_SHORT).show();
+            }
+        }
 
 
     }
